43dacfb5b237d6a401bd734db1a70da7eb2aae51
Grupo 4 <>
2020-12-04 22:18:38 +0100

    Intento 1009234

--- barometro/src/main/java/com/mycompany/barometro/ControladorBarometro.java
+++ barometro/src/main/java/com/mycompany/barometro/ControladorBarometro.java
@@ -1,72 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.barometro;
 
+import java.time.LocalDateTime;
+
 /**
- * @version 6
+ * @version 1
  * @author Grupo 4
  */
 public class ControladorBarometro {
-
-    
     private ModeloBarometro modelo;
     private VistaBarometro vista;
+    public static double presion;
+    public static String fecha;
     
     public ControladorBarometro(VistaBarometro vista){
         this.vista = vista;
         this.modelo = new ModeloBarometro();
     }
     
-    public static double presion;
-    public static String fecha;
-    public static String hora;
-    public static boolean tiempo;
+    public void setAltitud(Double altitud){
+        modelo.setAltitud(altitud);
+    }
     
-    static void getTiempo() {
-        if(tiempo == true){
-            System.out.println("Hace buen tiempo");
-        }else{
-            if(tiempo == false){
-                System.out.println("Hace mal tiempo");
-            }
-        }
+    public int getPrediccion(){
+        return(modelo.prediccion());
     }
-    public boolean isTiempo(){
-        return tiempo;
-    }
-    public double getPresion(){
-        return presion;
-    }
-    public void setPresion(double presion){
-        this.presion = presion;
-    }
-    public String getHora(){
-        return hora;
-    }
-    public void setHora(String hora){
-        this.hora = hora;
-    }
-    public String getFecha(){
-        return fecha;
-    }
-    public void setFecha(String fecha){
-        this.fecha = fecha;
-    }
-    public class Options {
-        private boolean all = false;
-        private boolean noBackup = false;
-        
-        public Options withAll() {
-            all = true;
-            return this;
-        }
-        
-        public Options withoutBackups() {
-            noBackup = true;
-            return this;
-        }
+    
+    public void addPresion(Double presion, LocalDateTime fecha){
+        modelo.addPresion(presion, fecha);
     }
 }
--- barometro/src/main/java/com/mycompany/barometro/VistaBarometro.java
+++ barometro/src/main/java/com/mycompany/barometro/VistaBarometro.java
@@ -1,60 +1,83 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.barometro;
-import java.io.File;
-import java.util.List;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import java.time.LocalDateTime;
 
 /**
- *@version 3
+ * @version 2
  * @author Grupo 4
  */
 public class VistaBarometro {
     private static ControladorBarometro control;
+    private static LocalDateTime fecha;
+    private static Double presion;
+    private static int predecir;
+    
     public VistaBarometro(){
         this.control = new ControladorBarometro(this);
     }
    
     public static void main(String args[]) {
+        VistaBarometro vista = new VistaBarometro();
+        vista.show(args);
+    }
+    
+    private void show(String args[]){
         HelpFormatter formatter = new HelpFormatter();
         
         Options opts = new Options();
-        opts.addOption("p", "presion");
-        opts.addOption("f", "all", false, 
-                " fecha .");
-        opts.addOption("h", "ignore-backups", false,
-                "hora");
+        opts.addOption("h", "Muestra opciones de ayuda");
+        opts.addOption("pr", "Muestra la predicción");
+        opts.addOption("pe", true, "Añade una nueva presión (también hay que añadir una fecha)");
+        opts.addOption("fh", true, "Añade una nueva fecha y una hora (también hay que añadir una presión)");
+        opts.addOption("al", true, "Cambia la altitud");
+        opts.addOption("G", true, "Poner la interfaz gráfica");
 
         CommandLineParser parser = new DefaultParser();
         
         try {
-            CommandLine cmd = parser.parse(opts, args);  
-            ControladorBarometro.Options cOpt = control.new Options();
-
-            if(cmd.hasOption("p")) {
-                cOpt.withAll();
-            }
+            CommandLine cmd = parser.parse(opts, args);
             
-            if(cmd.hasOption("f")) {
-                cOpt.withoutBackups();
+            if(cmd.hasOption("h")){
+                formatter.printHelp("VistaBarometro", opts);
             }
-            
-            if(cmd.hasOption("h")) {
-                
+            if(cmd.hasOption("pe") && cmd.hasOption("fh")) {
+                presion = Double.parseDouble(cmd.getOptionValue("p"));
+                fecha = LocalDateTime.parse(cmd.getOptionValue("fh"));
+                control.addPresion(presion, fecha);
+                System.out.println("Se ha añadido una nueva presión.");
             }
-            
-            ControladorBarometro.getTiempo();
+            if(cmd.hasOption("pr")){
+                predecir = control.getPrediccion();
+                switch(predecir){
+                    case 1:
+                        System.out.println("Va a haber una borrasca, pero pasará lejos.");
+                        break;
+                    case 2:
+                        System.out.println("Va a haber una gran borrasca.");
+                        break;
+                    case 3:
+                        System.out.println("Va a haber un tiempo seco y encalmado de anticiclón.");
+                        break;
+                    case 4:
+                        System.out.println("Va a haber una mejoría que puede ser pasajera, por tratarse del paso de una dorsal anticiclónica entre dos borrascas.");
+                        break;
+                    case 5:
+                        System.out.println("No va a haber grandes cambios en el clima.");
+                        break;
+                }
+            }
+            if(cmd.hasOption("al")){
+                control.setAltitud(Double.parseDouble(cmd.getOptionValue("al")));
+            }
            
         } catch (ParseException ex) {
-            formatter.printHelp( "ls", opts );
+            formatter.printHelp("VistaBarometro", opts );
         }
     }
 }
